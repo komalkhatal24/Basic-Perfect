71d483a1d7ccc232c2d9d3bb15ff90de2f494fe4
Komal Khatal <komalkhatal24@gmail.com>
2024-07-02 21:52:27 +0530

    Commit

---
+++ Floydtriangle .cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=5;
+    int start=1;
+    for (int i=1;i<=n;i++){
+    for(int j=1;j<i+i;j++){
+        cout<<start;
+        start++;
+       }
+       cout<<endl;
+    }
+    return 0;
+}
