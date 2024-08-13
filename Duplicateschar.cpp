c7215769e1a9f55e7cc82540a7fdc2a5a168524d
Komal Khatal <komalkhatal24@gmail.com>
2024-08-13 21:23:43 +0530

    commit

---
+++ Duplicateschar.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+void printDuplicates(string str){
+    sort(str.begin(),str.end());
+    
+    for(int i=0;i<str.length();i++){
+        int count=1;
+       if(i<str.length()-1 && str[i]==str[i+1]){
+       count++;
+       i++;
+    
+    }
+    if(count>1){
+        cout<<str[i]<<" count= "<<count<<endl;
+    }
+    }
+}
+int main(){
+    string str;
+    cin>>str;
+    printDuplicates(str);
+    return 0;
+}
